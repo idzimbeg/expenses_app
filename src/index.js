0bdcff71f2c6537ed679278a7915267abb56b97a
idzimbeg <igordzimbeg@gmail.com>
2021-10-15 12:00:41 +0200

    added WebVitals

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+reportWebVitals();
